7677eb153630c09467ab96dd3871bc322478e576
Jenua Boiko <jeyboy@bigmir.net>
2016-08-08 16:12:39 +0300

    add parser

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'grape-swagger'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug', platform: :mri
+  # gem 'byebug', platform: :mri
 
   gem 'rspec'
   # gem 'airborne'
--- app/controllers/api/tags.rb
+++ app/controllers/api/tags.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 module API
   class Tags < Grape::API
     resource :tags do
@@ -9,7 +11,22 @@ module API
       end
 
       get :parse do
-        # parse page from url
+        i = 0
+        html_text = open(params['url'])
+        doc = ::Nokogiri::HTML.parse(html_text)
+
+        ActiveRecord::Base.transaction do
+          doc.css('h1,h2,h3,a').map do |tag|
+            body = case tag.name
+              when 'a' then tag['href']
+              else tag.text
+            end
+
+            Tag.create!(body: body)
+          end
+        end
+
+        i = 0
       end
 
       # desc 'Return a tag'
